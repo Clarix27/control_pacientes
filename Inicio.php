1de974d377e3dde4c890c3ce3b661f40cb76a03a
Clarix27 <clarissaestrada8@gmail.com>
2025-06-25 23:06:50 -0700

    Actualizacion

--- Inicio.php
+++ Inicio.php
@@ -1,3 +1,13 @@
+<?php
+  session_start();
+  // Verificar si el usuario ha iniciado sesión
+  if (!isset($_SESSION['pk_usuario'])) {
+    // Redirigir a la página de login si no está autenticado
+    echo("<script>window.location.assign('Login.html');</script>");
+    exit();
+  }
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
